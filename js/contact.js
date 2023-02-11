8c796fe1fa33fb41fef6f8ed4b3dbb7aad475088
Dev <jaiswaldevraj337@gmail.com>
2023-02-11 12:06:54 +0530

    separate header and footer page created

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,11 @@
+
+$(document).ready(function () {
+  $('#header').load('header.html');
+  $('#footer').load('footer.html');
+}
+)
+
 // aos animation scroll initializing for on load scrolling
 AOS.init({
-    offset: 100,
-  });
+  offset: 100,
+});
