42750f030022ebca5924c253f17941af41c619cf
Dev <jaiswaldevraj337@gmail.com>
2023-02-16 10:48:02 +0530

    common header and footer added in video editing and graphics design page

--- js/video.js
+++ js/video.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+  $('#header-section').load('header.html');
+  $('#footer-section').load('footer.html');
+}
+)
 // aos animation scroll initializing for on load scrolling 
 AOS.init({
   offset: 50
